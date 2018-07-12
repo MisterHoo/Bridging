73fcc77e733786a6f4d4a52b80292984dc90eea9
Yosua Hoo <yosu4.hoo@gmail.com>
2018-07-12 18:11:41 +0700

    Initial Commit

---
+++ BridgeMC2-MC3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BridgeMC2-MC3
+//
+//  Created by Yosua Hoo on 12/07/18.
+//  Copyright © 2018 Yosua Hoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
